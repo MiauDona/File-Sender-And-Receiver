b1bb6279254bfe3ce8c1312b682169b57cdc816e
MiauDona <donanferyt@gmail.com>
2023-09-15 19:56:19 +0200

    Finished serverside. Need to fix to compile

--- src/main/java/miau/dona/Server.java
+++ src/main/java/miau/dona/Server.java
@@ -1,7 +1,10 @@
 package miau.dona;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,8 +23,27 @@ public class Server implements Runnable {
                 // Si acepta se queda esperando a que le lleguen archivos
                 if (result == JOptionPane.OK_OPTION) {
                     JOptionPane.showMessageDialog(null, "El cliente se ha conectado desde: " + clientSocket.getInetAddress().getHostAddress(), "Conexion realizada", JOptionPane.PLAIN_MESSAGE);
-                    // TODO: Sistema de recepcion de archivos
 
+                    // Empieza el inputstream de lo que envia el cliente
+                    InputStream inputStream = clientSocket.getInputStream();
+
+                    //Pregunta el nombre y la ruta del archivo para crearlo
+                    String fileName = JOptionPane.showInputDialog("Que nombre le quieres poner al archivo? \n Debes incluir la extension");
+                    String saveDirectory = JOptionPane.showInputDialog("En que directorio quieres guardar el archivo?");
+                    FileOutputStream fileOutputStream = new FileOutputStream(saveDirectory + fileName);
+
+                    // Fijar el buffer de entrada y leer los datos del archivo en trozos de 512MB
+                    byte[] buffer = new byte[536870912];
+                    int bytesRead;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        fileOutputStream.write(buffer, 0, bytesRead);
+                    }
+
+                    fileOutputStream.close();
+                    inputStream.close();
+                    clientSocket.close();
+
+                    JOptionPane.showMessageDialog(null, "Archivo recibido y guardado exitosamente");
 
                 } else {
                     JOptionPane.showMessageDialog(null, "Se ha rechazado la conexion", "Conexion fallida", JOptionPane.PLAIN_MESSAGE);
