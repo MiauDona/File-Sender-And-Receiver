e309302462931151e46b406d598c205d596a5748
MiauDona <donanferyt@gmail.com>
2023-09-15 02:27:16 +0200

    Full client messagery complete and part of server

---
+++ src/java/main/miau/dona/Client.java
@@ -0,0 +1,26 @@
+package main.miau.dona;
+
+import javax.swing.*;
+import java.io.*;
+import java.net.Proxy;
+import java.net.Socket;
+
+public class Client {
+        public static void main() {
+            //Elegir direccion IP
+            String IP = JOptionPane.showInputDialog("Donde te quieres conectar? (IP)");
+            try (Socket clientSocket = new Socket(IP, 38528); // Intenta conectarse a la IP
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Crea el input
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);) {  // Crea el output
+
+               BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in)); // Envia lo que el usuario escribe en la consola
+               String message;
+
+
+
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Esa direccion IP no existe o no está conectada", "Error -1", JOptionPane.ERROR_MESSAGE);
+            }
+
+        }
+    }
